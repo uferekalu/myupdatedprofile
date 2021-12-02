26f8c75d2fe8bee032836d85c39a3e45663405e9
Ufere Kalu <dekalusha@gmail.com>
2021-12-01 23:42:03 -0600

    adds search functionality

--- src/screens/ProjectsScreen.js
+++ src/screens/ProjectsScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import "./ProjectsScreen.css";
 
@@ -10,6 +10,7 @@ import Project from "../components/Project";
 import { getProjects as listProjects } from "../redux/actions/projectAction";
 
 const ProjectsScreen = () => {
+  const [search, setSearch] = useState("");
   const dispatch = useDispatch();
 
   const getProjects = useSelector(state => state.getProjects);
@@ -23,27 +24,48 @@ const ProjectsScreen = () => {
     [dispatch]
   );
 
+  const filteredProjects =
+    search.length === 0
+      ? projects
+      : projects.filter(project =>
+          project.name.toLowerCase().includes(search.toLowerCase())
+        );
+
   return (
     <div className="projectscreen">
       <h2 className="projectscreen__title">Projects</h2>
+      <input
+        type="text"
+        placeholder="Search project..."
+        className="search__project"
+        value={search}
+        onChange={e => setSearch(e.target.value)}
+      />
       <div className="projects__list">
         {loading
-          ? <h2><i className="fas fa-spinner fa-spin fa-3x"></i></h2>
+          ? <h2>
+              <i className="fas fa-spinner fa-spin fa-2x" />
+            </h2>
           : error
             ? <h2>
                 {error}
               </h2>
-            : projects.map(project =>
-                <Project
-                  key={project._id}
-                  projectId={project._id}
-                  name={project.name}
-                  weburl={project.weburl}
-                  repourl={project.repourl}
-                  img={project.img}
-                  description={project.description}
-                />
-              )}
+            : filteredProjects
+                .sort(
+                  (a, b) =>
+                    a.name.toUpperCase() > b.name.toUpperCase() ? 1 : -1
+                )
+                .map(project =>
+                  <Project
+                    key={project._id}
+                    projectId={project._id}
+                    name={project.name}
+                    weburl={project.weburl}
+                    repourl={project.repourl}
+                    img={project.img}
+                    description={project.description}
+                  />
+                )}
       </div>
     </div>
   );
